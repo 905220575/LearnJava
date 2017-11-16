9b20bc1d58fa44587ac98605e5cc1624ea95a063
905220575 <905220575@qq.com>
2017-11-16 09:44:32 +0800

    FileName of FCFSandSjf

---
+++ src/FCFSandSJF/FCFSTestTwo.java
@@ -0,0 +1,11 @@
+package zhonghe;
+
+public class FCFSTestTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FCFStwo fcfs = new FCFStwo();
+		fcfs.Run("FileList.txt");
+	}
+
+}
---
+++ src/FCFSandSJF/SJFTestTwo.java
@@ -0,0 +1,11 @@
+package zhonghe;
+
+public class SJFTestTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SJFtwo sjf = new SJFtwo();
+		sjf.Run("FileList.txt");
+	}
+
+}
